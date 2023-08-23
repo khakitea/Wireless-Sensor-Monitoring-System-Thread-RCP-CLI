ff41cd200f3fb539b59f4149e1fbcf1c66ab954b
Robyn <robyn.jaebin.jung@gmail.com>
2023-08-23 12:18:26 -0700

    final submission for showcase

---
+++ STM32CubeIDE/Application/MAX6675.h
@@ -0,0 +1,17 @@
+/*************************************************************************************
+ Title	 :  MAXIM Integrated MAX6675 Library for STM32 Using HAL Libraries
+ Author  :  Bardia Alikhan Afshar <bardia.a.afshar@gmail.com>
+ Software:  STM32CubeIDE
+ Hardware:  Any STM32 device
+*************************************************************************************/
+#ifndef MAX6675_H_
+#define MAX6675_H_
+#include "main.h"
+
+// ------------------------- Defines -------------------------
+#define SSPORT GPIOA       // GPIO Port of Chip Select(Slave Select)
+#define SSPIN  GPIO_PIN_4  // GPIO PIN of Chip Select(Slave Select)
+
+// ------------------------- Functions  ----------------------
+float Max6675_Read_Temp(void);
+#endif
